67d2f1797228f7a98c1a7eadc540e1798a449bcf
1a2a3a4a5 <8804who@naver.com>
2025-07-21 23:23:35 +0900

    feat(chat): add get_answer to chat_service and update chatbot get_answer logic

--- backend/api/v1/endpoints/chatbot.py
+++ backend/api/v1/endpoints/chatbot.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter, HTTPException
+from services.chat_service import get_chat_model, get_answer
 from schemas.chat import ChatRequest
-from services.chat_service import get_chat_model
 import logging
 
 chatbot_router = APIRouter()
-
-chat_model = get_chat_model()
 
 
 @chatbot_router.post("/chat")
@@ -23,8 +21,8 @@ async def chat(request: ChatRequest) -> dict:
         HTTPException: 챗봇 응답 중 오류가 발생한 경우 500 에러 반환
     """
     try:
-        response = chat_model.invoke(request.message)
-        return {"message": response}
+        answer = get_answer(user_query=request.message)
+        return {"message": answer}
     except Exception as e:
         logging.error(f"Error in chat: {e}")
         raise HTTPException(status_code=500, detail=str(e))
--- backend/services/chat_service.py
+++ backend/services/chat_service.py
@@ -1,6 +1,8 @@
-from langchain_openai import ChatOpenAI
 from core import config
+from fastapi import HTTPException
+from langchain_openai import ChatOpenAI
 from pydantic import SecretStr
+import logging
 
 OPENAI_API_KEY = config.OPENAI_API_KEY
 
@@ -9,3 +11,13 @@ def get_chat_model():
     if OPENAI_API_KEY is None:
         raise ValueError("OPENAI_API_KEY is not set.")
     return ChatOpenAI(api_key=SecretStr(OPENAI_API_KEY), model="gpt-4o-mini")
+
+
+def get_answer(user_query: str):
+    try:
+        model = get_chat_model()
+        answer = model.invoke(user_query)
+        return answer
+    except Exception as e:
+        logging.error(f"Error in chat: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
