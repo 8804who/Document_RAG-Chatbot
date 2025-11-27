b835d1c8d1e5d64869046b9d16deae480160bff9
8804 <8804who@naver.com>
2025-11-27 23:50:08 +0900

    modify(depricated model import): declarative_base form sqlalchemy.orm because sqlalchemy.ext.devlarative_base was deprecatred

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,9 +1,8 @@
 from contextlib import contextmanager, asynccontextmanager
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker, declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
 
 from app.core import config
 
