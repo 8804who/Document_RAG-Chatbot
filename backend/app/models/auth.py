b8ccf653ae2fc6a46e35afff3837b0e476288255
8804 <8804who@naver.com>
2025-12-04 23:47:19 +0900

    modify(db): revise table architecture

--- backend/app/models/auth.py
+++ backend/app/models/auth.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Text
 
 from app.db.database import Base
 
@@ -7,4 +7,4 @@ class GoogleOauth(Base):
     __tablename__ = "google_oauth"
     email = Column(String(255), primary_key=True)
     name = Column(String(255))
-    refresh_token = Column(String(255))
+    refresh_token = Column(Text)
--- backend/app/models/session_id.py
+++ backend/app/models/session_id.py
@@ -5,5 +5,5 @@ from app.db.database import Base
 
 class SessionId(Base):
     __tablename__ = "session_id"
-    email = Column(String(255), ForeignKey("google_oauth.email"), primary_key=True)
-    session_id = Column(String(255))
+    email = Column(String(100), ForeignKey("google_oauth.email"), primary_key=True)
+    session_id = Column(String(100))
